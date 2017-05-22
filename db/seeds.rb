cd130e10f6aae8d09a6e82b28551d285f0e1cffa
Kylan Ishii <kylanishii@gmail.com>
2017-05-21 21:40:56 -0700

    seeds6

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,22 +26,27 @@
 #     )
 # end
 #
-10.times do
-    User.create(
-        username:  Faker::Name.name,
-        full_name: Faker::Friends.character,
-        password: Faker::Friends.character,
-        full_address: Faker::Address.street_address,
-        is_admin: Faker::Boolean.boolean(0.2),
-        email_address: Faker::Internet.email,
-        password_digest: ""
-    )
-end
-#
+
+User.create([
+    {username: "pizza2", full_name: "bob aid", password: "1234", full_address: "1234 poodle str.", is_admin: false, email_address: "hello@world.com"},
+    {username: "hamburger2", full_name: "cool aid", password: "abcd", full_address: "1234 hound str.", is_admin: false, email_address: "bye@world.com"}
+    ])
+# 10.times do
+#     User.create(
+#         username:  Faker::Name.name,
+#         full_name: Faker::Friends.character,
+#         # password: Faker::Friends.character,
+#         full_address: Faker::Address.street_address,
+#         is_admin: Faker::Boolean.boolean(0.2),
+#         email_address: Faker::Internet.email,
+#         password_digest: ""
+#     )
+# end
+# #
 #
 10.times do
     UserEvent.create(
-        user_id: Faker::Number.between(1, 10),
+        user_id: Faker::Number.between(1, 2),
         user_event_name: Faker::Zelda.game,
         user_event_date: Faker::Date.forward(23),
         start_time: Faker::Time.between(2.days.ago, Date.today, :morning),
