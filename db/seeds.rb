4cd440fae670d07448bb332906e4a74384205bb5
Kylan Ishii <kylanishii@gmail.com>
2017-05-21 21:31:28 -0700

    seeds4

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,39 +13,39 @@
 #     )
 # end
 
+# 10.times do
+#     Event.create(
+#         region_id: Faker::Number.between(1, 4),
+#         event_name: Faker::Zelda.game,
+#         event_date: Faker::Date.forward(23),
+#         start_time: Faker::Time.between(2.days.ago, Date.today, :morning),
+#         end_time: Faker::Time.between(2.days.ago, Date.today, :afternoon),
+#         category: Faker::SlackEmoji.people,
+#         regions: Faker::Name.title,
+#         traffic: Faker::Number.number(3)
+#     )
+# end
+#
 10.times do
-    Event.create(
-        region_id: Faker::Number.between(1, 4),
-        event_name: Faker::Zelda.game,
-        event_date: Faker::Date.forward(23),
-        start_time: Faker::Time.between(2.days.ago, Date.today, :morning),
-        end_time: Faker::Time.between(2.days.ago, Date.today, :afternoon),
-        category: Faker::SlackEmoji.people,
-        regions: Faker::Name.title,
-        traffic: Faker::Number.number(3)
+    User.create(
+        username:  Faker::Name.name,
+        full_name: Faker::Friends.character,
+        password: Faker::Friends.character,
+        full_address: Faker::Address.street_address,
+        is_admin: Faker::Boolean.boolean(0.2),
+        email_address: Faker::Internet.email,
+        password_digest: ""
     )
 end
 #
-# 10.times do
-#     User.create(
-#         username:  Faker::Name.name,
-#         full_name: Faker::Friends.character,
-#         password: Faker::Friends.character,
-#         full_address: Faker::Address.street_address,
-#         is_admin: Faker::Boolean.boolean(0.2),
-#         email_address: Faker::Internet.email,
-#         password_digest: ""
-#     )
-# end
 #
-#
-# 10.times do
-#     UserEvent.create(
-#         user_id: Faker::Number.between(1, 100),
-#         user_event_name: Faker::Zelda.game,
-#         date: Faker::Date.forward(23),
-#         start_time: Faker::Time.between(2.days.ago, Date.today, :morning),
-#         end_time: Faker::Time.between(2.days.ago, Date.today, :afternoon),
-#         category: Faker::SlackEmoji.people
-#     )
-# end
+10.times do
+    UserEvent.create(
+        user_id: Faker::Number.between(1, 100),
+        user_event_name: Faker::Zelda.game,
+        user_event_date: Faker::Date.forward(23),
+        start_time: Faker::Time.between(2.days.ago, Date.today, :morning),
+        end_time: Faker::Time.between(2.days.ago, Date.today, :afternoon),
+        category: Faker::SlackEmoji.people
+    )
+end
