7dae05d65e4471b0d1d39a76cb1e9e7ae51b0e67
Kylan Ishii <kylanishii@gmail.com>
2017-05-22 01:07:52 -0700

    be2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4040', 'https://calendar-app-api.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
